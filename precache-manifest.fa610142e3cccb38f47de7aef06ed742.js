2daff7555b8bbfb2f55d3a11dd82cb688ccd1cb8
Sourav <srvds7th@hotmail.com>
2020-07-19 03:41:26 +0530

    Updates

---
+++ precache-manifest.fa610142e3cccb38f47de7aef06ed742.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb78fc7cbfa766d4a39906af49b3460",
+    "url": "/task_manager/index.html"
+  },
+  {
+    "revision": "153b94a7da9b4208432d",
+    "url": "/task_manager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b4a298307465da29850",
+    "url": "/task_manager/static/js/2.76182d7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task_manager/static/js/2.76182d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153b94a7da9b4208432d",
+    "url": "/task_manager/static/js/main.fb527270.chunk.js"
+  },
+  {
+    "revision": "f3f34df1062219b4595a",
+    "url": "/task_manager/static/js/runtime-main.2a80a336.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task_manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
